6206aa2fc1fa871665e8e1e21726b32b9b62e2a8
unknown <jagadeeshsandopu@gmail.com>
2021-05-17 17:52:52 +0530

    edit

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,20 @@
 import React,{useContext, useState} from "react"
 import {Globalcontext} from './context/globalstate'
+import ReorderIcon from '@material-ui/icons/Reorder';
 import {Link} from 'react-router-dom'
+
 import "./header.css"
 
 const Header = () => {
    const {watchlist} = useContext(Globalcontext);
+   const [show,setshow] =useState(false);
     return (
       <header className = "container">
           
           <h1> MOVIE APP</h1>
-          <ul className= 'links'>
+          
+          <ul className= 'links' id={show?"hidden":""} >
+             
              <li>
                 <Link to ="/" className="link">Watchlist</Link>
              </li>
@@ -22,6 +27,7 @@ const Header = () => {
                 <Link to ="/add" className="link">Add</Link>
              </li>
           </ul>
+          <button onClick={() =>{setshow(!show)}}><ReorderIcon/></button>
          
       </header>
     )
